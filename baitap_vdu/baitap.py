8d9bdbd77d6ea5aec785ec27b52cf78a3fa4c979
DTH235714_lethiyennhi <phuthuynhogemini@gmail.com>
2025-08-29 10:13:36 +0700

    nop bai tp chuong 1

---
+++ baitap_vdu/baitap.py
@@ -0,0 +1,40 @@
+import math
+import os
+import sys
+
+import module1
+import module2 
+
+import math_libs
+print(math_libs.add(4,5))
+
+print('Hello World!')
+
+print('Albert Einstein đã từng nói: Một người không bao giờ mắc' \
+' sai lầm không bao giờ thử bất kỳ điều gì mới.')
+
+if 5>2:
+    print('Five is greater than two!')
+
+total = 1 + \
+2 + \
+33
+print(total); print('Hello Python!')
+
+if True:
+    print('True')
+else:
+    print('False')
+
+#This is a comment
+print('Hello, World!')
+
+print("Tham so 1: ",sys.argv[1])
+print("Tham so 2: ",sys.argv[2])
+print("Tham so 3: ",sys.argv[3])
+# python baitap.py 1 2 3 
+
+print(module1.question)
+print(module2.question)
+print(module1.answer)
+print(module2.answer)
